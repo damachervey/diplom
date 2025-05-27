a86365215a43c15a63fbdb6e40776d723e21bc47
damachervey <145565305+damachervey@users.noreply.github.com>
2025-05-27 16:14:18 +0300

    Update burger.js

--- burger.js
+++ burger.js
@@ -1,15 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM загружен');
     const burgerMenu = document.getElementById('burger-menu');
     const navMenu = document.getElementById('navMenu');
 
-    burgerMenu.addEventListener('click', (event) => {
-        event.stopPropagation();
+    burgerMenu.addEventListener('click', () => {
+        console.log('Бургер-меню нажато');
         burgerMenu.classList.toggle('active');
         navMenu.classList.toggle('active');
     });
 
     document.addEventListener('click', (event) => {
         if (!navMenu.contains(event.target) && !burgerMenu.contains(event.target)) {
+            console.log('Клик вне меню');
             burgerMenu.classList.remove('active');
             navMenu.classList.remove('active');
         }
